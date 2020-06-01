0d54f42965530dd92962153a47d263a0d70ddfa4
Yamartim <>
2020-06-01 18:22:28 -0300

    aula 1
    codigo da primeira aula do segundo curso  de python

---
+++ c2aula1.py
@@ -0,0 +1,20 @@
+def play():
+
+	print("----------------------------------")
+	print("the games of forca shall begin")
+	print("-------------------------------gay")
+
+
+	palavra = 'gambiarra'
+	
+	perdeu = False
+	acertou = False
+	
+	#enquanto o jogador nao perder e nao acertar a palavra o jogo continua....
+	while(not perdeu and not acertou): #todas as palavras chave em python são em letra minuscula, True e False são excessões pois são valores
+		print("continuando game loop...")
+
+	print("GAME OVER")
+	
+if(__name__ == __main__):
+	play()
